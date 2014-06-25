4048336fa87c42ae3523efbe7a2a1c664acea145
Anton Lundin <glance@acc.umu.se>
2014-06-25 10:11:52 +0200

    Upgrade to libusb-1.0.18

--- build.sh
+++ build.sh
@@ -112,23 +112,21 @@ if [ ! -e $PKG_CONFIG_PATH/libgit2.pc ] ; then
 	popd
 fi
 
-if [ ! -e libusb-1.0.9.tar.bz2 ] ; then
-	wget http://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-1.0.9/libusb-1.0.9.tar.bz2
+if [ ! -e libusb-1.0.18.tar.bz2 ] ; then
+	wget http://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-1.0.18/libusb-1.0.18.tar.bz2
 fi
-if [ ! -e libusb-1.0.9 ] ; then
-	tar -jxf libusb-1.0.9.tar.bz2
-fi
-if ! grep -q __ANDROID__ libusb-1.0.9/libusb/io.c ; then
-	# patch libusb to build with android-ndk
-	patch -p1 < libusb-1.0.9-android.patch  libusb-1.0.9/libusb/io.c
+if [ ! -e libusb-1.0.18 ] ; then
+	tar -jxf libusb-1.0.18.tar.bz2
 fi
 if [ ! -e $PKG_CONFIG_PATH/libusb-1.0.pc ] ; then
 	mkdir -p libusb-build-$ARCH
 	pushd libusb-build-$ARCH
-	../libusb-1.0.9/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
+	../libusb-1.0.18/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --disable-udev
 	make
 	make install
 	popd
+	# Bugfix stupid -c flag
+	perl -pi -e 's/^(Libs.private:.*)-c(.*)$/$1$2/' $PKG_CONFIG_PATH/libusb-1.0.pc
 fi
 
 if [ ! -e libdivecomputer/configure ] ; then
