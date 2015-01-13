5ca77d8ca9d6ec4d11466c516f618a9bb5ac06ae
Anton Lundin <glance@acc.umu.se>
2015-01-13 22:41:00 +0100

    Abstract version numbers out to config varables
    Signed-off-by: Anton Lundin <glance@acc.umu.se>

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@ export ANDROID_SDK_ROOT=$PWD/../android-sdk-linux
 export QT5_ANDROID=$PWD/../Qt/5.4
 export ANDROID_NDK_HOST=linux-x86
 
+# Which versions are we building against?
+SQLITE_VERSION=3080704
+LIBXML2_VERSION=2.9.2
+LIBXSLT_VERSION=1.1.28
+LIBZIP_VERSION=0.11.2
+LIBGIT2_VERSION=0.21.2
+LIBUSB_VERSION=1.0.19
+
 # arm or x86
 export ARCH=${1-arm}
 
@@ -35,31 +43,31 @@ if [ ! -e subsurface/CMakeLists.txt ] || [ ! -e libdivecomputer/configure.ac ] ;
 	git submodule update
 fi
 
-if [ ! -e sqlite-autoconf-3080704.tar.gz ] ; then
-	wget http://www.sqlite.org/2014/sqlite-autoconf-3080704.tar.gz
+if [ ! -e sqlite-autoconf-${SQLITE_VERSION}.tar.gz ] ; then
+	wget http://www.sqlite.org/2014/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
 fi
-if [ ! -e sqlite-autoconf-3080704 ] ; then
-	tar -zxf sqlite-autoconf-3080704.tar.gz
+if [ ! -e sqlite-autoconf-${SQLITE_VERSION} ] ; then
+	tar -zxf sqlite-autoconf-${SQLITE_VERSION}.tar.gz
 fi
 if [ ! -e $PKG_CONFIG_PATH/sqlite3.pc ] ; then
 	mkdir -p sqlite-build-$ARCH
 	pushd sqlite-build-$ARCH
-	../sqlite-autoconf-3080704/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
+	../sqlite-autoconf-${SQLITE_VERSION}/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
 	make -j4
 	make install
 	popd
 fi
 
-if [ ! -e libxml2-2.9.2.tar.gz ] ; then
-	wget ftp://xmlsoft.org/libxml2/libxml2-2.9.2.tar.gz
+if [ ! -e libxml2-${LIBXML2_VERSION}.tar.gz ] ; then
+	wget ftp://xmlsoft.org/libxml2/libxml2-${LIBXML2_VERSION}.tar.gz
 fi
-if [ ! -e libxml2-2.9.2 ] ; then
-	tar -zxf libxml2-2.9.2.tar.gz
+if [ ! -e libxml2-${LIBXML2_VERSION} ] ; then
+	tar -zxf libxml2-${LIBXML2_VERSION}.tar.gz
 fi
 if [ ! -e $PKG_CONFIG_PATH/libxml-2.0.pc ] ; then
 	mkdir -p libxml2-build-$ARCH
 	pushd libxml2-build-$ARCH
-	../libxml2-2.9.2/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --without-python --without-iconv --enable-static --disable-shared
+	../libxml2-${LIBXML2_VERSION}/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --without-python --without-iconv --enable-static --disable-shared
 	perl -pi -e 's/runtest\$\(EXEEXT\)//' Makefile
 	perl -pi -e 's/testrecurse\$\(EXEEXT\)//' Makefile
 	make -j4
@@ -67,61 +75,62 @@ if [ ! -e $PKG_CONFIG_PATH/libxml-2.0.pc ] ; then
 	popd
 fi
 
-if [ ! -e libxslt-1.1.28.tar.gz ] ; then
-	wget ftp://xmlsoft.org/libxml2/libxslt-1.1.28.tar.gz
+if [ ! -e libxslt-${LIBXSLT_VERSION}.tar.gz ] ; then
+	wget ftp://xmlsoft.org/libxml2/libxslt-${LIBXSLT_VERSION}.tar.gz
 fi
-if [ ! -e libxslt-1.1.28 ] ; then
-	tar -zxf libxslt-1.1.28.tar.gz
-	cp libxml2-2.9.2/config.sub libxslt-1.1.28
+if [ ! -e libxslt-${LIBXSLT_VERSION} ] ; then
+	tar -zxf libxslt-${LIBXSLT_VERSION}.tar.gz
+	# libxslt have too old config.sub for android
+	cp libxml2-${LIBXML2_VERSION}/config.sub libxslt-${LIBXSLT_VERSION}
 fi
 if [ ! -e $PKG_CONFIG_PATH/libxslt.pc ] ; then
 	mkdir -p libxslt-build-$ARCH
 	pushd libxslt-build-$ARCH
-	../libxslt-1.1.28/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --with-libxml-prefix=${PREFIX} --without-python --without-crypto --enable-static --disable-shared
+	../libxslt-${LIBXSLT_VERSION}/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --with-libxml-prefix=${PREFIX} --without-python --without-crypto --enable-static --disable-shared
 	make
 	make install
 	popd
 fi
 
-if [ ! -e libzip-0.11.2.tar.gz ] ; then
-	wget http://www.nih.at/libzip/libzip-0.11.2.tar.gz
+if [ ! -e libzip-${LIBZIP_VERSION}.tar.gz ] ; then
+	wget http://www.nih.at/libzip/libzip-${LIBZIP_VERSION}.tar.gz
 fi
-if [ ! -e libzip-0.11.2 ] ; then
-	tar -zxf libzip-0.11.2.tar.gz
+if [ ! -e libzip-${LIBZIP_VERSION} ] ; then
+	tar -zxf libzip-${LIBZIP_VERSION}.tar.gz
 fi
 if [ ! -e $PKG_CONFIG_PATH/libzip.pc ] ; then
 	mkdir -p libzip-build-$ARCH
 	pushd libzip-build-$ARCH
-	../libzip-0.11.2/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
+	../libzip-${LIBZIP_VERSION}/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
 	make
 	make install
 	popd
 fi
 
-if [ ! -e libgit2-0.21.2.tar.gz ] ; then
-	wget -O libgit2-0.21.2.tar.gz https://github.com/libgit2/libgit2/archive/v0.21.2.tar.gz
+if [ ! -e libgit2-${LIBGIT2_VERSION}.tar.gz ] ; then
+	wget -O libgit2-${LIBGIT2_VERSION}.tar.gz https://github.com/libgit2/libgit2/archive/v${LIBGIT2_VERSION}.tar.gz
 fi
-if [ ! -e libgit2-0.21.2 ] ; then
-	tar -zxf libgit2-0.21.2.tar.gz
+if [ ! -e libgit2-${LIBGIT2_VERSION} ] ; then
+	tar -zxf libgit2-${LIBGIT2_VERSION}.tar.gz
 fi
 if [ ! -e $PKG_CONFIG_PATH/libgit2.pc ] ; then
 	mkdir -p libgit2-build-$ARCH
 	pushd libgit2-build-$ARCH
 	# -DCMAKE_CXX_COMPILER=arm-linux-androideabi-g++
-	cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_VERSION=Android -DCMAKE_C_COMPILER=${CC} -DCMAKE_FIND_ROOT_PATH=${PREFIX} -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DANDROID=ON -DSHA1_TYPE=builtin -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ../libgit2-0.21.2/
+	cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_VERSION=Android -DCMAKE_C_COMPILER=${CC} -DCMAKE_FIND_ROOT_PATH=${PREFIX} -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DANDROID=ON -DSHA1_TYPE=builtin -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ../libgit2-${LIBGIT2_VERSION}/
 	make
 	make install
 	popd
 fi
 
-if [ ! -e libusb-1.0.19.tar.gz ] ; then
-	wget -O libusb-1.0.19.tar.gz https://github.com/libusb/libusb/archive/v1.0.19.tar.gz
+if [ ! -e libusb-${LIBUSB_VERSION}.tar.gz ] ; then
+	wget -O libusb-${LIBUSB_VERSION}.tar.gz https://github.com/libusb/libusb/archive/v${LIBUSB_VERSION}.tar.gz
 fi
-if [ ! -e libusb-1.0.19 ] ; then
-	tar -zxf libusb-1.0.19.tar.gz
+if [ ! -e libusb-${LIBUSB_VERSION} ] ; then
+	tar -zxf libusb-${LIBUSB_VERSION}.tar.gz
 fi
-if [ ! -e libusb-1.0.19/configure ] ; then
-	pushd libusb-1.0.19
+if [ ! -e libusb-${LIBUSB_VERSION}/configure ] ; then
+	pushd libusb-${LIBUSB_VERSION}
 	mkdir m4
 	autoreconf -i
 	popd
@@ -129,7 +138,7 @@ fi
 if [ ! -e $PKG_CONFIG_PATH/libusb-1.0.pc ] ; then
 	mkdir -p libusb-build-$ARCH
 	pushd libusb-build-$ARCH
-	../libusb-1.0.19/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --disable-udev
+	../libusb-${LIBUSB_VERSION}/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --disable-udev
 	make
 	make install
 	popd
